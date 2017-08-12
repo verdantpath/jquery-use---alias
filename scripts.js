ecc7ae13fbd6263d95b25431fa83bdf892b2049c
verdantpath <m_dittmann@yahoo.com>
2017-08-12 15:33:05 -0700

    add js

---
+++ scripts.js
@@ -0,0 +1,3 @@
+(function($) { //function to create private scope with $ parameter
+  // private scope and use $ without fear of conflict
+})(jQuery); // invoke nameless function and pass it the jQuery object
